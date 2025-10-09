e26f3bb604b1b46e35767c3f0a12d8b2765f71ce
kerangtomes-web <kerangtomes@gmail.com>
2025-10-10 00:53:31 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Change this token with your bot token. You can get it from @botfather (https://t.me/botfather)
+ * also change this chat_id with your chat_id. To get your chat_id, you can
+ * use @id_chat_bot (https://t.me/id_chat_bot) in telegram. DONT FORGET TO START THE BOT.   
+ */
+$config = [
+    'telegram_bot_token' => '7320558671:AAFC1shFm36EUB3xqVp2KGZTuVyP_8fUgdo',
+    'chat_id'            => '6155453169'
+];
